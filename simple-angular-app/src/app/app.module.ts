3ce43b35d5758bf2a55f550c2411047c8ddc5608
caio.portugal <caioportugal92@gmail.com>
2019-11-16 20:06:46 -0300

    Simple-angular-app - Adicionando o arquivo main

--- simple-angular-app/src/app/app.module.ts
+++ simple-angular-app/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import {routes} from './routers';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
